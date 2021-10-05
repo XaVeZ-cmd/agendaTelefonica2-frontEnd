5635bc3637361899e8ed5aaed230a219c1c8519f
XaVeZ-cmd <thiago.amorim_2@live.com>
2021-10-05 10:46:12 -0300

    Criaçao do header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './views/components/templates/header/header.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 
 
@@ -11,11 +14,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
