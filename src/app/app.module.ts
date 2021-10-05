c46a8217d0e284e8f598af09705cb49a0f86b853
XaVeZ-cmd <thiago.amorim_2@live.com>
2021-10-05 12:40:31 -0300

    Criaçao da navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './views/components/templates/header/header.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from './views/components/templates/footer/footer.component';
+import { NavbarComponent } from './views/components/templates/navbar/navbar.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatMenuModule} from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 
 
@@ -17,12 +24,17 @@ import { FooterComponent } from './views/components/templates/footer/footer.comp
     AppComponent,
     HeaderComponent,
     FooterComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
